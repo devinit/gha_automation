54b32b4df0c9bb5920af7f79d569852863752047
danjwalton <20973540+danjwalton@users.noreply.github.com>
2022-02-24 15:48:47 +0000

    Update informAPI.R

--- general/informAPI.R
+++ general/informAPI.R
@@ -1,13 +1,12 @@
-inform_get <- function(){
+inform_get <- function(year, indicator){
   lapply(c("data.table", "jsonlite"), require, character.only = T)
   
-  year <- year(Sys.Date())
   wf_url <- "https://drmkc.jrc.ec.europa.eu/inform-index/API/InformAPI/Workflows/GetByYear/"
   year_wf_id <- data.table(fromJSON(paste0(wf_url, year)))
   wf_id <- year_wf_id[which.max(as.Date(WorkflowDate))]$WorkflowId
   
-  inform_out <- data.table(fromJSON(paste0("https://drmkc.jrc.ec.europa.eu/inform-index/API/InformAPI/countries/Scores/?WorkflowId=", wf_id)))
-  inform_out <- inform_out[ValidityYear != 0]
+  inform_out <- data.table(fromJSON(paste0("https://drmkc.jrc.ec.europa.eu/inform-index/API/InformAPI/countries/Scores/?WorkflowId=", wf_id, "&IndicatorId=",indicator)))
+  inform_out[, year := year]
   
   return(inform_out)
 }
