256786f8b90ad21706246095cd23ef6151b61686
danjwalton <20973540+danjwalton@users.noreply.github.com>
2022-05-12 00:07:22 +0100

    Update fts_sohs_master.R

--- IHA/fts_sohs_master.R
+++ IHA/fts_sohs_master.R
@@ -6,16 +6,13 @@ fts_save_master <- function(years, update_years = update_years, path = "IHA/data
   }
 }
 
-fts_read_master <- function(years = years, extra_columns = NULL){
-  main_columns <- c("id", "year", "destinationObjects_Plan.name", "description", "donor_country_iso3", "donor_country", "sourceObjects_Organization.name", "source_org_type", "recipient", "iso3", "destinationObjects_Organization.name", "gha_channel", "channel", "subchannel", "multiyear", "dummy", "status", "amountUSD_defl")
-  columns <- c(main_columns, extra_columns)
+fts_read_master <- function(years = years){
   fts_curated_all <- list()
   for(i in 1:length(years)){
     year <- years[i]
-    gh_url <- paste0("https://raw.githubusercontent.com/devinit/gha_automation/main/IHA/datasets/fts_curated_", year, ".csv")
+    gh_url <- paste0("https://raw.githubusercontent.com/devinit/gha_automation/main/IHA/datasets/fts_curated_master/fts_curated_", year, ".csv")
     fts_curated_all[[i]] <- fread(gh_url, showProgress = F, encoding = "UTF-8")
   }
   fts_curated_all <- rbindlist(fts_curated_all)
-  fts_curated_all <- fts_curated_all[, ..columns]
   return(fts_curated_all)
 }
