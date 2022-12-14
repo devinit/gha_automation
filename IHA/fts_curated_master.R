32f1740b792ad8e12728e2ab9e77d1c3521170c6
danjwalton <20973540+danjwalton@users.noreply.github.com>
2022-12-14 14:43:09 +0000

    fts update 14-12-22

---
+++ IHA/fts_curated_master.R
@@ -0,0 +1,19 @@
+fts_save_master <- function(years, update_years = update_years, path = "IHA/datasets/fts_curated_master/"){
+  source("https://raw.githubusercontent.com/devinit/gha_automation/main/IHA/fts_curated_flows.R")
+  fts_all <- fts_curated_flows(years = years, update_years = update_years)
+  for(i in 1:length(years)){
+    fwrite(fts_all[year == years[[i]]], paste0(path, "fts_curated_", years[[i]], ".csv"))
+  }
+}
+
+fts_read_master <- function(years = years){
+  fts_curated_all <- list()
+  for(i in 1:length(years)){
+    year <- years[i]
+    gh_url <- paste0("https://raw.githubusercontent.com/devinit/gha_automation/main/IHA/datasets/fts_curated_master/fts_curated_", year, ".csv")
+    fts_curated_all[[i]] <- fread(gh_url, showProgress = F, encoding = "UTF-8")
+  }
+  fts_curated_all <- rbindlist(fts_curated_all)
+  fts_curated_all <- fts_curated_all[, ..columns]
+  return(fts_curated_all)
+}
